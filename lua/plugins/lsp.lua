b6087ebce52d3f6c0a3f6c0aacb3f46454c69941
Developer <developer@example.com>
2026-06-21 01:39:59 +0700

    clippy rush

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,14 @@
 vim.pack.add({ "https://github.com/neovim/nvim-lspconfig", "https://github.com/WieeRd/auto-lsp.nvim" })
 -- vim.lsp.enable("lua_ls")
 -- vim.lsp.enable("nim_langserver")
-vim.lsp.enable("ols")
--- require("auto-lsp").setup()
+--
+vim.lsp.config("rust_analyzer", {
+	settings = {
+		["rust-analyzer"] = {
+			check = {
+				command = "clippy",
+			},
+		},
+	},
+})
+require("auto-lsp").setup()
